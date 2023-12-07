2e40565c290f449da4213ffa1ae1b5bf14473def
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2023-12-08 01:29:19 +0900

    Title: 숫자 차례로 출력하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231208/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,23 @@
+# 변수 선언 및 입력:
+n = int(input())
+
+
+def print_number1(n):
+    if n == 0:
+        return
+    
+    print_number1(n - 1)
+    print(n, end=" ")
+
+
+def print_number2(n):
+    if n == 0:
+        return
+
+    print(n, end=" ")
+    print_number2(n - 1)
+
+
+print_number1(n)
+print()
+print_number2(n)

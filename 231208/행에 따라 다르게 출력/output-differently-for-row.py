8c1e4d82140c68df4d3547b7c57df844a876bdf5
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2023-12-08 01:32:54 +0900

    Title: 행에 따라 다르게 출력, Time: 137ms, Memory: 26MB, Status: Passed - CodeTree

--- 231208/행에 따라 다르게 출력/output-differently-for-row.py
+++ 231208/행에 따라 다르게 출력/output-differently-for-row.py
@@ -1,22 +1,36 @@
+# 변수 선언 및 입력
 n = int(input())
-
 cnt = 0
+	
+# 값을 조건대로 증가시켜 출력합니다.
 for i in range(n):
-    if i % 2 == 0 :
-        for j in range(n):
-            # if j == 0 :
-            #     cnt += 1
-            print(cnt, end = ' ')
-            cnt += 1 
-        # cnt += 1
-    else :
-        for j in range(n):  
-            # if j == 0 :
-            #     cnt += 1
-            print(cnt, end = ' ') 
-            cnt += 2   
-        # cnt -= 1
+    for j in range(n):
+        if i % 2 == 0:
+            cnt += 1
+        else:
+            cnt += 2
+        print(cnt, end=" ")
     print()
+
+# n = int(input())
+
+# cnt = 0
+# for i in range(n):
+#     if i % 2 == 0 :
+#         for j in range(n):
+#             # if j == 0 :
+#             #     cnt += 1
+#             print(cnt, end = ' ')
+#             cnt += 1 
+#         # cnt += 1
+#     else :
+#         for j in range(n):  
+#             # if j == 0 :
+#             #     cnt += 1
+#             print(cnt, end = ' ') 
+#             cnt += 2   
+#         # cnt -= 1
+#     print()
      
 #   0  1  2  3 
 # 0 1  2  3  4

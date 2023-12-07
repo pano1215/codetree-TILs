41984f46608d21aa24ea239a02af6d5352c9c998
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2023-12-08 01:24:04 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231208/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 231208/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,41 +1,30 @@
 # 변수 선언 및 입력:
-a, b = tuple(map(int, input().split()))
+n1, n2 = tuple(map(int, input().split()))
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
 
-# 해당 숫자가 소수인지 여부를 반환하는 함수를 작성합니다.
-def is_prime(n):
-    if n == 1:
-        return False
-    
-    for i in range(2, n):
-        if n % i == 0:
+# n번째 인덱스부터 시작하는 n2길이의 a수열과
+# b수열이 완전히 일치하는지 확인합니다..
+def is_same(n):
+    for i in range(n2):
+        if a[i + n] != b[i]:
             return False
-    
+
     return True
 
 
-# 자릿수의 합이 짝수인지 여부를 반환하는 함수를 작성합니다.
-def is_digit_sum_even(n):
-    digit_sum = (n // 100) + ((n // 10) % 10) + (n % 10)
-    if digit_sum % 2 == 0:
-        return True
+# b가 a의 연속부분수열인지 확인합니다.
+def is_subsequence():
+    for i in range(n1 - n2 + 1):
+        if is_same(i):
+            return True
     
     return False
 
 
-# 해당 숫자가 소수이면서 자릿수의 합이 짝수인지 여부를 반환하는 함수를 작성합니다.
-def judge_num(n):
-    if is_prime(n) and is_digit_sum_even(n):
-        return True
-    
-    return False
-
-
-cnt = 0
-
-for i in range(a, b + 1):
-    if judge_num(i):
-        cnt += 1
-
-
-print(cnt)
+if is_subsequence():
+    print("Yes")
+else:
+    print("No")

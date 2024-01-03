9e587beebd57230ff3a6fc7c08b9f70ae961caf0
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-03 17:20:13 +0900

    Title: 실수 입력받아 계산, Time: 277ms, Memory: 13MB, Status: Passed - CodeTree

--- 240103/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 240103/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split("\n");
 
-let a = input[0];
-let b = input[1];
-let re = Number(a + b) ;
+let a = Number(input[0]);
+let b = Number(input[1]);
+let re = a + b ;
 
 console.log(re.toFixed(2));

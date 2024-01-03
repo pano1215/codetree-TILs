782c4250220ef7da0274f01519ee40445e0ff9bd
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-03 17:28:48 +0900

    Title: 실수와 문자 받아 출력하기, Time: 238ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240103/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -0,0 +1,11 @@
+// 변수 선언, 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let c = input[0];
+let b = Number(input[1]);
+let a = Number(input[2]);
+
+// 출력
+console.log(c)
+console.log(b.toFixed(2));
+console.log(a.toFixed(2));

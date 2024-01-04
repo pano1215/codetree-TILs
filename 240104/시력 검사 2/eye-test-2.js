d38edc4a139ad53ed77f791529247eb518bd2fba
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 10:53:47 +0900

    Title: 시력 검사 2, Time: 280ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/시력 검사 2/eye-test-2.js
@@ -0,0 +1,16 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let a = Number(input);
+
+// 출력
+if (a >= 1.0) {
+    console.log("High");
+}
+else if (a >= 0.5) {
+    console.log("Middle");
+}
+else {
+    console.log("Low");
+}

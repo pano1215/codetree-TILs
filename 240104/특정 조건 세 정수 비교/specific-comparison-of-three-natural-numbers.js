f042115558f135f75bd504ff85e39bdc00c87cfc
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 13:45:06 +0900

    Title: 특정 조건 세 정수 비교, Time: 317ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240104/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,26 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let resA, resB;
+
+// 출력
+if (a <= b && a <= c) {
+    resA = 1;
+}
+else {
+    resA = 0;
+}
+
+if (a == b && b == c) {
+    resB = 1;
+}
+else {
+    resB = 0;
+}
+
+console.log(resA, resB);

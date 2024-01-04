07f8c874038373124abb272fb45730f55d7a0b0a
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 17:04:18 +0900

    Title: 문자열 출력하기, Time: 295ms, Memory: 13MB, Status: Passed - CodeTree

--- 240104/문자열 출력하기/print-string.js
+++ 240104/문자열 출력하기/print-string.js
@@ -1,10 +1,8 @@
 // 변수 선언 및 입력
 const fs = require("fs");
-let n = fs.readFileSync(0).toString().trim();
+let n = Number(fs.readFileSync(0).toString().trim());
 
 // 출력
-let result = "";
-for (let i = 0; i < 8; i++) {
-    result += n;
+for (let i = 0; i < n; i++) {
+    console.log("LeebrosCode");
 }
-console.log(result);

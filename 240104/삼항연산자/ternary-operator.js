cfc1b300eeac51aebbae3888fcd247e1c0aec600
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 10:51:57 +0900

    Title: 삼항연산자, Time: 225ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/삼항연산자/ternary-operator.js
@@ -0,0 +1,10 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let a = Number(input);
+
+let result = a === 100 ? "pass" : "failure";
+
+// 출력
+console.log(result);

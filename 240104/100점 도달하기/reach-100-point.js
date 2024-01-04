c3ee8fedb82e96e9b518d34cdb0e0ce625e81dcf
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 17:28:19 +0900

    Title: 100점 도달하기, Time: 262ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/100점 도달하기/reach-100-point.js
@@ -0,0 +1,25 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = "";
+
+// 출력
+for (let i = n; i <= 100; i++) {
+    if (i >= 90) {
+        result += "A ";
+    }
+    else if (i >= 80) {
+        result += "B ";
+    }
+    else if (i >= 70) {
+        result += "C ";
+    }
+    else if (i >= 60) {
+        result += "D ";
+    }
+    else {
+        result += "F ";
+    }
+}
+
+console.log(result);

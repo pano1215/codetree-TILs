86ea8ff4eb5914f15b7234ecdf1d7302eda23453
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 10:48:11 +0900

    Title: 2개 중 최대, Time: 320ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/2개 중 최대/max-of-two-nums.js
@@ -0,0 +1,11 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = a > b ? a : b;
+
+// 출력
+console.log(result);

fcfe133f3fb680696eaaab2bde505fd07a6eb455
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 17:57:27 +0900

    Title: 청소당번 정하기, Time: 235ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/청소당번 정하기/cleaning-numbering.js
@@ -0,0 +1,19 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cntA = 0, cntB = 0, cntC = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 12 === 0) {
+        cntC++;
+    }
+    else if (i % 3 === 0) {
+        cntB++;
+    }
+    else if (i % 2 === 0) {
+        cntA++;
+    }
+}
+
+// 출력
+console.log(cntA, cntB, cntC);

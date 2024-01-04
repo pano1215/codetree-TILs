cc8fc9abad92ec8e6af3c5537505b29548375914
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-04 11:24:32 +0900

    Title: 특정 조건 두 정수 비교, Time: 241ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240104/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -0,0 +1,25 @@
+// 입력 및 변수 선언
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+// 출력
+let resultA, resultB;
+
+if (a < b) {
+    resultA = 1;
+} 
+else {
+    resultA = 0;
+}
+
+if (a === b) {
+    resultB = 1;
+} 
+else {
+    resultB = 0;
+}
+
+console.log(resultA, resultB);

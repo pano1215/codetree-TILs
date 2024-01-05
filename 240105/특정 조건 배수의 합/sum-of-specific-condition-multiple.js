bed7977a5164c6ed5956c87dd348ebd80b9d0399
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-05 10:54:10 +0900

    Title: 특정 조건 배수의 합, Time: 282ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240105/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
@@ -0,0 +1,24 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let sumVal = 0;
+
+if (a < b) {
+    for (let i = a; i <= b; i++) {
+        if (i % 5 === 0) {
+            sumVal += i;
+        }
+    }
+}
+else {
+    for (let i = b; i <= a; i++) {
+        if (i % 5 === 0) {
+            sumVal += i;
+        }
+    } 
+}
+
+// 출력
+console.log(sumVal);

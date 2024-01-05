78dfe0f12b17c88642aaf0d4e33bc2303012d74d
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-05 12:35:07 +0900

    Title: 친근하지 않은 수, Time: 284ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240105/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,13 @@
+const fs = require("fs") ;
+let input = fs.readFileSync(0).toString().trim() ;
+let n = Number(input) ;
+let cnt = 0 ;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0) {
+        continue ;
+    }
+    cnt += 1 ;
+}
+
+console.log(cnt) ;

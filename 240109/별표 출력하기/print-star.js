08ec36f722d7235d2f3791ec6c47ac2081c23cdb
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-09 17:08:52 +0900

    Title: 별표 출력하기, Time: 308ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240109/별표 출력하기/print-star.js
@@ -0,0 +1,23 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+// 길이가 n인 직각삼각형을 출력합니다.
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j <= i; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
+
+// 길이가 n-1인 직각삼각형을 뒤집어 출력합니다.
+for (let i = n - 2; i >= 0; i--) {
+    str = "";
+    for (let j = 0; j <= i; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}

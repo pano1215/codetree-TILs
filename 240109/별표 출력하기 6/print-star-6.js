67ce3309d0c3c7a8f56d8f08dff18d7710db38ad
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-09 17:33:43 +0900

    Title: 별표 출력하기 6, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240109/별표 출력하기 6/print-star-6.js
@@ -0,0 +1,31 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+// 모양에 맞게 윗쪽 별을 출력합니다.
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < i; j++) {
+        str += "  ";
+    }
+
+    for (let j = 0; j < (2 * n) - (2 * i) - 1; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
+
+// 모양에 맞게 아랫쪽 별을 출력합니다.
+for (let i = n - 2; i >= 0; i--) {
+    str = "";
+    for (let j = 0; j < i; j++) {
+        str += "  ";
+    }
+
+    for (let j = 0; j < (2 * n) - (2 * i) - 1; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}

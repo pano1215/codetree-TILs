e284b1207afee8c87f5c74b4a32a7a8933276b66
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-12 12:45:36 +0900

    Title: 평균점수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/평균점수 구하기/print-average.js
@@ -0,0 +1,10 @@
+const fs = require("fs") ;
+let input = fs.readFileSync(0).toString().trim().map(Number) ;
+
+let total = 0 ;
+
+for (let e of input) {
+    total += e ;
+}
+let result = total / 8 ;
+console.log(result.toFixed(1)) ;

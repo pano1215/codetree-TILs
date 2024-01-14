9646c00c182c44739f5b3901abf59373144c2553
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-14 20:55:03 +0900

    Title: 기울어진 직사각형, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240114/기울어진 직사각형/slanted-rectangle.py
+++ 240114/기울어진 직사각형/slanted-rectangle.py
@@ -11,29 +11,28 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 def is_range(x, y) :
     return 0 <= x and x < n and 0 <= y and y < n
 
-def get_score(x, y, k, l):
-    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
-    move_nums = [k, l, k, l]
+# def get_score(x, y, k, l):
+#     dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+#     move_nums = [k, l, k, l]
     
-    sum_of_nums = 0
+#     sum_of_nums = 0
 
-    # 기울어진 직사각형의 경계를 쭉 따라가봅니다.
-    for dx, dy, move_num in zip(dxs, dys, move_nums):
-        for _ in range(move_num):
-            x, y = x + dx, y + dy
+#     # 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+#     for dx, dy, move_num in zip(dxs, dys, move_nums):
+#         for _ in range(move_num):
+#             x, y = x + dx, y + dy
                 
-            # 기울어진 직사각형이 경계를 벗어나는 경우라면
-            # 불가능하다는 의미로 답이 갱신되지 않도록
-            # 0을 반환합니다.
-            if not is_range(x, y):
-                return 0
+#             # 기울어진 직사각형이 경계를 벗어나는 경우라면
+#             # 불가능하다는 의미로 답이 갱신되지 않도록
+#             # 0을 반환합니다.
+#             if not is_range(x, y):
+#                 return 0
             
-            sum_of_nums += arr[x][y]
+#             sum_of_nums += arr[x][y]
     
-    return sum_of_nums
+#     return sum_of_nums
 
 max_num = -sys.maxsize
-
 for row in range(n) : 
     for column in range(n) :
         # route_sum = 0
@@ -43,26 +42,34 @@ for row in range(n) :
 
         for upper in range(1, n) : # 가로길이
             for height in range(1, n) : # 세로길이
-                max_num = max(max_num, get_score(row, column, upper, height))
-print(max_num)
-                # move_nums = [upper, height, upper, height]
-                # dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
-                # temp_x = 0 
-                # boolean = False
-
-                # for dx, dy, move_num in zip(dxs, dys, move_nums):
-                #     for _ in range(move_num) :
-                #         row, column = row + dx, column + dy
-                        
-                #         if is_range(row, column) :
-                #             temp_x += arr[row][column]
-                #             #print(row, column, upper, height)
-                #         else :
-                #             row, column = row - dx, column - dy
-                #             temp_x = 0
-                #             print('111111111111111111111111111111111111111')
-                #             continue 
                 
+                move_nums = [upper, height, upper, height]
+                dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+                temp_x = 0 
+                boolean = False
+
+                for dx, dy, move_num in zip(dxs, dys, move_nums):
+                    for _ in range(move_num) :
+                        row, column = row + dx, column + dy
+
+                        if boolean :
+                            break
+                        
+                        if is_range(row, column) :
+                            temp_x += arr[row][column]
+                            #print(row, column, upper, height)
+                        else :
+                            row, column = row - dx, column - dy
+                            #print(row, column, upper, height, temp_x)
+                            boolean = True
+                            temp_x = 0
+                            continue 
+                    # else:
+                    #     continue
+                    # break
+                max_num = max(max_num, temp_x)
+                
+print(max_num)
 
                 # route_sum = 0
                 # temp_x = arr[rr][cc]

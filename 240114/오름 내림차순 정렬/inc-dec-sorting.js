5094703f36695e1d471ea7a6abdedc334fe284da
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-14 09:27:51 +0900

    Title: 오름 내림차순 정렬, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240114/오름 내림차순 정렬/inc-dec-sorting.js
@@ -0,0 +1,25 @@
+function cmp(prev, cur) {
+    if (prev > cur) return 1;
+    else if (prev < cur) return -1;
+    return 0;
+}
+
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0]) ;
+let arr = input[1].trim().split(" ").map(Number) ;
+
+let sort_arr = arr.sort(cmp) ;
+let sort_result = " " ;
+for (let i of sort_arr) {
+    sort_result += i + " " ;
+}
+console.log(sort_result) ;
+
+let reverse_arr = arr.reverse() ;
+let reverse_result = " " ;
+for (let i of reverse_arr) {
+    reverse_result += i + " " ;
+}
+console.log(reverse_result) ;

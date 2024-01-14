3ad1cc518eb17a9f1b6e246b8d82b4a147bfae61
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-14 09:43:48 +0900

    Title: 오름 내림차순 정렬, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

--- 240114/오름 내림차순 정렬/inc-dec-sorting.js
+++ 240114/오름 내림차순 정렬/inc-dec-sorting.js
@@ -11,14 +11,14 @@ let n = Number(input[0]) ;
 let arr = input[1].trim().split(" ").map(Number) ;
 
 let sort_arr = arr.sort(cmp) ;
-let sort_result = " " ;
+let sort_result = "" ;
 for (let i of sort_arr) {
     sort_result += i + " " ;
 }
 console.log(sort_result) ;
 
 let reverse_arr = arr.reverse() ;
-let reverse_result = " " ;
+let reverse_result = "" ;
 for (let i of reverse_arr) {
     reverse_result += i + " " ;
 }

744cc6c1d660a82feb03b27d4d74e3ed416840cb
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-18 22:53:57 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240118/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,30 +1,41 @@
 n, r, c = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
+dxs = [-1, 1, 0, 0]
+dys = [0, 0, -1, 1]
+
+cur_x, cur_y = r - 1, c - 1
+compare_num = arr[cur_x][cur_y]
+visited_arr = [] 
+visited_arr.append(compare_num)
+
 def is_range(x, y) :
     return 0 <= x and x < n and 0 <= y and y < n
 
 def move() :
-    dxs = [-1, 1, 0, 0]
-    dys = [0, 0, -1, 1]
-
-    cur_x, cur_y = r - 1, c - 1
-    compare_num = arr[cur_x][cur_y]
-    visited_arr = [compare_num]
-
     for dx, dy in zip(dxs, dys) :
+        global next_x, next_y, cur_x, cur_y, compare_num
         next_x, next_y = cur_x + dx, cur_y + dy
         #print(dx, dy)
+        #print(next_x, next_y)
         if is_range(next_x, next_y) and arr[next_x][next_y] > compare_num :
-            #print('33333')
-            visited_arr.append(arr[next_x][next_y])
+            #print(next_x, next_y)
+            # visited_arr.append(arr[next_x][next_y])
             compare_num = arr[next_x][next_y]
+
             cur_x = next_x
             cur_y = next_y
-    
-    return visited_arr
 
-result = move()
+            return True 
+       
+    return False
 
-for e in result :
+while True :
+
+    if not move() :
+        break
+
+    visited_arr.append(arr[next_x][next_y])
+
+for e in visited_arr :
     print(e, end = ' ')

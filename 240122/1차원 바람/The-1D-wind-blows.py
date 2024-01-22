f9b96ebd9dc3bf58ea16799c20e2565dbbad6b19
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-22 21:11:03 +0900

    Title: 1차원 바람, Time: 256ms, Memory: 27MB, Status: Passed - CodeTree

--- 240122/1차원 바람/The-1D-wind-blows.py
+++ 240122/1차원 바람/The-1D-wind-blows.py
@@ -2,11 +2,11 @@ n, m, q = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 wind = [list(map(str, input().split())) for _ in range(q)]
 
-# 배열 범위 체크 함수
-def row_plus_in_range(x):
+# 배열 범위 체크 함수 row + 1 / row - 1
+def row_in_range(x):
     return 0 <= x < n 
 
-# 배열 범위 체크 함수
+# 배열 범위 체크 함수 row - 1
 def row_mius_in_range(x):
     return 0 <= x < n 
 
@@ -15,7 +15,7 @@ def Up_check(row, arr) :
 
     check = False
     for j in range(m) :
-        if row_mius_in_range(row - 1) and arr[row - 1][j] == arr[row][j] :
+        if row_in_range(row - 1) and arr[row - 1][j] == arr[row][j] :
             check = True
     row = row - 1
     return check
@@ -25,64 +25,74 @@ def Down_check(row, arr) :
 
     check = False
     for j in range(m) :
-        if row_plus_in_range(row + 1) and arr[row + 1][j] == arr[row][j] :
+        if row_in_range(row + 1) and arr[row + 1][j] == arr[row][j] :
             check = True
     row = row + 1
     return check
 
-def Left_move(row) :
-    last_temp = arr[row][-1] 
+# def Left_move(row) :
+#     last_temp = arr[row][-1] 
     
-    for i in range(m - 1, 0, -1) :
-        arr[row][i] = arr[row][i - 1]
-    arr[row][0] = last_temp
-    return arr
+#     for i in range(m - 1, 0, -1) :
+#         arr[row][i] = arr[row][i - 1]
+#     arr[row][0] = last_temp
+#     return arr
 
 def Move(row, dirt) :
     if dirt == 'L' :
         last_temp = arr[row][-1]
         for i in range(m - 1, 0, -1) :
+
             arr[row][i] = arr[row][i - 1]
         arr[row][0] = last_temp
+        dirt = 'R'
     else :
         first_temp = arr[row][0]
-        for i in range(1, m) :
+        for i in range(0, m - 1) :
             arr[row][i] = arr[row][i + 1]
         arr[row][-1] = first_temp
+        dirt = 'L'
     return arr, dirt
 
-def Right_move(row) :
-    first_temp = arr[row][0] 
-    for i in range(1, m) :
-        arr[row][i] = arr[row][i + 1]
-    arr[row][-1] = first_temp
-    return arr
+# def Right_move(row) :
+#     first_temp = arr[row][0] 
+#     for i in range(1, m) :
+#         arr[row][i] = arr[row][i + 1]
+#     arr[row][-1] = first_temp
+#     return arr
 
 row = 0
 for i in range(q) :
     row = int(wind[i][0]) - 1
     dirt = wind[i][1] 
-    
-    while(True) :
-        arr, dirt = Move(row, dirt)
-    #if dirt == 'L' :
-    #    arr = Left_move(row) 
-    #    dirt == 'R'
-    #else :
-    #    arr = Right_move(row) 
-    #    dirt == 'L'
 
-        if Up_check(row, arr) and Down_check(row, arr) :
-            print('둘 다 true')
-        elif Up_check(row, arr) :
-            print('Up_check true')
-            row = row - 1
+    arr, dirt = Move(row, dirt)
 
-        elif Down_check(row, arr) :
-            print('Down_check true')
-            row = row + 1
+    up_row = row
+    up_dirt = dirt
+
+    down_row = row
+    down_dirt = dirt
+
+    while(True) : # up
+        if Up_check(up_row, arr) :
+            up_row -= 1
+            #print('Up_check true 들어가기 전 : ', up_dirt)
+            arr, up_dirt = Move(up_row, up_dirt)
+            #print('Up_check true : ', up_dirt)
         else :
-            print('둘 다 false')
+            break
+    
+    while(True) : # down
+        if Down_check(down_row, arr) :
+            down_row += 1
+            #print('Down_check true 들어가기 전 : ', down_dirt)
+            arr, down_dirt = Move(down_row, down_dirt)
+            #print('Down_check true : ', down_dirt)
+        else :
             break
 
-print(arr)
+for e in  arr :
+    for ee in e :
+        print(ee, end = ' ')
+    print()

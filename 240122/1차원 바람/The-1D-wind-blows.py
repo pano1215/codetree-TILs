dac558783d59856bd065b7176d8908c103b12e3b
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-22 17:52:56 +0900

    Title: 1차원 바람, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/1차원 바람/The-1D-wind-blows.py
+++ 240122/1차원 바람/The-1D-wind-blows.py
@@ -11,7 +11,7 @@ def row_mius_in_range(x):
     return 0 <= x < n 
 
 def Up_check(row, arr) :
-    global row
+    #global row
 
     check = False
     for j in range(m) :
@@ -21,8 +21,8 @@ def Up_check(row, arr) :
     return check
 
 def Down_check(row, arr) :
-    global row 
-    
+    #global row 
+
     check = False
     for j in range(m) :
         if row_plus_in_range(row + 1) and arr[row + 1][j] == arr[row][j] :
@@ -49,7 +49,7 @@ def Move(row, dirt) :
         for i in range(1, m) :
             arr[row][i] = arr[row][i + 1]
         arr[row][-1] = first_temp
-    return arr
+    return arr, dirt
 
 def Right_move(row) :
     first_temp = arr[row][0] 
@@ -58,12 +58,13 @@ def Right_move(row) :
     arr[row][-1] = first_temp
     return arr
 
+row = 0
 for i in range(q) :
     row = int(wind[i][0]) - 1
     dirt = wind[i][1] 
     
     while(True) :
-        arr = Move(row, dirt)
+        arr, dirt = Move(row, dirt)
     #if dirt == 'L' :
     #    arr = Left_move(row) 
     #    dirt == 'R'
@@ -75,10 +76,13 @@ for i in range(q) :
             print('둘 다 true')
         elif Up_check(row, arr) :
             print('Up_check true')
+            row = row - 1
 
         elif Down_check(row, arr) :
             print('Down_check true')
+            row = row + 1
         else :
             print('둘 다 false')
+            break
 
 print(arr)

d9cbd045546c583036a26bc8f0f809070d107102
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-01-26 12:48:37 +0900

    Title: 숫자의 순차적 이동, Time: 252ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240126/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -0,0 +1,55 @@
+import sys
+
+# 입력
+n, t = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# 선언
+dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
+dys = [0, 1, 1, 1, 0, -1, -1, -1]
+
+# 격자 안에 있는 범위 탐색
+def is_in_range(x, y) :
+    return 0 <= x < n and 0 <= y < n
+
+# 교체하기
+def Exchange(max_num, num) :
+    max_row, max_col = Search_point(max_num)
+    row, col = Search_point(num)
+
+    temp = arr[max_row][max_col]
+    arr[max_row][max_col] = arr[row][col]
+    arr[row][col] = temp
+
+    return arr
+
+
+# 8방향 중 최대값 찾기 
+def Search_dirt(row, col) :
+    max_num = -sys.maxsize
+    for dx, dy in zip(dxs, dys) :
+        next_x, next_y = row + dx, col + dy
+
+        if is_in_range(next_x, next_y) :
+            if arr[next_x][next_y] > max_num :
+                max_num = arr[next_x][next_y]
+    return max_num
+
+# 각 값들의 위치(r, c) 찾기 
+def Search_point(num) :
+    for row in range(n) :
+        for col in range(n) :
+            if arr[row][col] == num :
+                return row, col
+
+for turn in range(t) :
+    # 1부터 n * n까지의 위치 찾기
+    for num in range(1, (n * n) + 1) :
+        row, col = Search_point(num) # 숫자의 위치찾기
+        max_num = Search_dirt(row, col) # 8방향 중 최대값 찾기
+        arr = Exchange(max_num, num)
+
+for e in arr :
+    for ee in e :
+        print(ee, end = ' ')
+    print()

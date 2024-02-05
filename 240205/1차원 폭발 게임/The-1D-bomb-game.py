ad8d48dfac28d0fa32c45f59094a7e0f31127eb1
BEB_05_한정은 <78583863+pano1215@users.noreply.github.com>
2024-02-05 12:51:29 +0900

    Title: 1차원 폭발 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240205/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240205/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,42 +1,31 @@
-# 변수 선언 및 입력
-n, m = tuple(map(int, input().split()))
-numbers = [
-    int(input())
-    for _ in range(n)
-]
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
 
+# m이상으로 연속하는 폭탄이 있는지 체크
+def Same_check(start_idx, curr_num) :
+    for i in range(start_idx + 1, len(arr)) :
+        if arr[start_idx] != curr_num  :
+            return i - 1
+    return len(arr) - 1
 
-# 주어진 시작점에 대하여
-# 부분 수열의 끝 위치를 반환합니다.
-def get_end_idx_of_explosion(start_idx, curr_num):
-    for end_idx in range(start_idx + 1, len(numbers)):
-        if numbers[end_idx] != curr_num:
-            return end_idx - 1
-        
-    return len(numbers) - 1
+while True :
+    same_check = False
+    cur_idx = 0
 
+    while cur_idx < len(arr) :
+        end_idx = Same_check(cur_idx, arr[cur_idx]) 
 
-while True:
-    did_explode = False
-    curr_idx = 0
-    
-    while curr_idx < len(numbers):
-        end_idx = get_end_idx_of_explosion(curr_idx, numbers[curr_idx])
-        
-        if end_idx - curr_idx + 1 >= m:
-            # 연속한 숫자의 개수가 m개 이상이면
-            # 폭탄이 터질 수 있는 경우 해당 부분 수열을 잘라내고
-            # 폭탄이 터졌음을 기록해줍니다.
-            del numbers[curr_idx:end_idx + 1]
-            did_explode = True
-        else:
-            # 주어진 시작 원소에 대하여 폭탄이 터질 수 없는 경우
-            # 다음 원소에 대하여 탐색하여 줍니다.
-            curr_idx = end_idx + 1
+        if end_idx - cur_idx + 1 >= m :
+            del arr[cur_idx : end_idx + 1]
+            same_check = True
+        else :
+            cur_idx = end_idx + 1
 
-    if not did_explode:
+    if not same_check :
         break
 
-print(len(numbers))
-for number in numbers:
-    print(number)
+# 정답 출력
+print(len(arr))
+
+for ca in arr :
+    print(ca)

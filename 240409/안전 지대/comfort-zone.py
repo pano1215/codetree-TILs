7f993cd0c6355f66c3440233eefd1b2234ee611f
한정은 <78583863+pano1215@users.noreply.github.com>
2024-04-09 08:43:14 +0900

    Title: 안전 지대, Time: 844ms, Memory: 112MB, Status: Passed - CodeTree

--- 240409/안전 지대/comfort-zone.py
+++ 240409/안전 지대/comfort-zone.py
@@ -40,24 +40,6 @@ def dfs(x, y) :
 
             if is_in_range(next_x, next_y) and village_copy[next_x][next_y] != 0 :
                 dfs(next_x, next_y)
-    
-    # 다음 위치의 값이 0이 아닌 경우에 이동
-    # if village_copy[x][y] != 0 :
-    #     # 이미 방문한 곳은 visted에서 True로 처리해서 방문하지 못하도록 함
-    #     visited[x][y] = True
-    #     village_copy[x][y] = 0
-    #     cnt += 1
-
-    #     print(k, cnt)
-    #     print(village_copy)
-    #     ## 네 방향 중에 전진 가능한 곳을 재호출 
-    #     for dx, dy in zip(dxs, dys) :
-    #         # next_x, next_y 세팅
-    #         next_x = x + dx
-    #         next_y = y + dy
-
-    #         if is_in_range(next_x, next_y) :
-    #             dfs(next_x, next_y)
 
 # k이하면 모두 0으로 처리하는 함수 
 def change_to_zero_less_k(k) :
@@ -65,8 +47,6 @@ def change_to_zero_less_k(k) :
         for col in range(m) :
             if village_copy[row][col] <= k :
                 village_copy[row][col] = 0
-    #print(k, village_copy)
-    #return village_copy
 
 result = [] # 높이(k)와 안전지대 수를 기록할 결과 배열 만들기
 # 1부터 +1씩 하면서 높이(K)를 입력해주기
@@ -81,13 +61,7 @@ while k <= max_height : # K가 최대 높이를 넘으면 종료
             if village_copy[row][col] != 0 and visited[row][col] == False:
                 dfs(row, col)
                 cnt += 1
-    #print(k, cnt)
-    #if not a :
-        # 이전 문제처럼 dfs가 끝날 때마다 cnt를 카운트해줘서 
-        # 안전지대(dfs가 갈 수 있는 구역)를 체크함
-    #    cnt = 0
 
-    #print(cnt)
     # 높이(k)와 안전지대 수를 기록함 
     result.append([k, cnt])
 

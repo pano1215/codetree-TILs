800f8b8f3b568ae0f5f05d34df588ee2f8dffc76
한정은 <78583863+pano1215@users.noreply.github.com>
2024-05-04 18:15:13 +0900

    Title: 정수 n개의 합 2, Time: 150ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240504/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,17 @@
+import sys
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+prefix_sum = [0] * n
+
+prefix_sum[0] = arr[0]
+for i in range(1, n) :
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+#print(prefix_sum)
+max_sum = -sys.maxsize
+for i in range(n - k) :
+    sum_sum = prefix_sum[i + k - 1] - prefix_sum[i] + arr[i]
+    #print(prefix_sum[i + k - 1], prefix_sum[i], arr[i], sum_sum)
+    max_sum = max(max_sum, sum_sum)
+print(max_sum)

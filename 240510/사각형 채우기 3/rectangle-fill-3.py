cf5d8dc018b2a232193758f5f7029f1d3d60bdfa
한정은 <78583863+pano1215@users.noreply.github.com>
2024-05-10 13:58:48 +0900

    Title: 사각형 채우기 3, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240510/사각형 채우기 3/rectangle-fill-3.py
+++ 240510/사각형 채우기 3/rectangle-fill-3.py
@@ -1,17 +1,14 @@
 n = int(input())
-memo = [-1] * (n * 2)
+memo = [-1] * 1000
 
 def test(n) :
-    memo[0] = 1
-    memo[1] = 1
+    memo[1] = 2
     memo[2] = 7
-    memo[3] = 22
-    memo[4] = 71
 
     if memo[n] != -1 :
         return memo[n]
     
-    if n >= 5 :
+    if n >= 3 :
         memo[n] = (test(n - 1) * 3) + test(n - 2) - test(n - 3)
     
     return memo[n]

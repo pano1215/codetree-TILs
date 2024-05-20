eb5d82e0a2b85197e4b2d9909ec7faa71277b3b9
한정은 <78583863+pano1215@users.noreply.github.com>
2024-05-20 12:11:29 +0900

    Title: 서로 다른 BST 개수 세기, Time: 151ms, Memory: 101MB, Status: Passed - CodeTree

--- 240520/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240520/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -2,9 +2,9 @@ n = int(input())
 dp = [0] * 10000000
 
 def test(n) : 
-    #dp[1] = 1
-    #dp[2] = 2
-    #dp[2] = 5
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 5
     
     if dp[n] != 0 :
         return dp[n]

e5a2d41c7ec43aad6fd8f6ab374b4b31f948e325
한정은 <78583863+pano1215@users.noreply.github.com>
2024-05-24 08:44:42 +0900

    Title: 겹치지 않게 선분 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240524/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,34 @@
+n = int(input())
+line_arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [False for _ in range(10)] # 일단 10으로 해두고 1000으로 바꿔서 제출하기  
+cnt = 0 # 안 겹치는 선분 수 cnt 
+
+# 겹치는지 확인하는 함수
+def overlab(x1, x2) :
+    return any(visited[i] for i in range(x1, x2 + 1))
+
+# 방문한 곳 True 처리
+def visited_true(x1, x2) :
+    for i in range(x1, x2 + 1) :
+        visited[i] = True
+    return visited
+
+def select_line(idx) :
+    global cnt 
+
+    if idx == n :
+        return 
+        
+    #print(visited)
+    x1, x2 = line_arr[idx]
+    if overlab(x1, x2) :
+        #print('겹침')
+        select_line(idx + 1)
+    else : 
+        #print('안 겹침')
+        visited_true(x1, x2)
+        select_line(idx + 1)
+        cnt += 1
+
+select_line(0)
+print(cnt)

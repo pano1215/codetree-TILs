f81ec74a1efbc9a9f4179dba55db83e7c6e9e5f8
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-10-10 02:07:41 +0900

    Title: 동전 더하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/동전 더하기/add-coins.py
+++ 241010/동전 더하기/add-coins.py
@@ -2,13 +2,9 @@ n, k = map(int, input().split())
 coin = [int(input()) for _ in range(n)]
 coin = coin[::-1]
 
-i, cnt = 0, 0 
+ans, i = 0, 0
 while k > 0 :
-    if k >= coin[i] :
-        k -= coin[i]
-        cnt += 1
-    else :
-        i += 1
-        k -= coin[i]
-        cnt += 1
-print(cnt)
+    ans += k // coin[i]
+    k %= coin[i]
+    i += 1
+print(ans)

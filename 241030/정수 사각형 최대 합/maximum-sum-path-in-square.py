990fa6e63d391ae5ebcd1d6b1e2c492cacda167f
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-10-30 15:58:44 +0900

    Title: 정수 사각형 최대 합, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 241030/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -5,11 +5,14 @@ arr2 = [[0 for _ in range(n)] for _ in range(n)]
 arr2[0][0] = arr1[0][0]
 
 # 가로 세로 값 설정하기 
-for row in range(n) :
-    for col in range(n) : 
-        if row == 0 :
-            arr2[0][col] += arr1[0][col] 
-        
-    arr2[row][0] += arr1[row][0]
+for row in range(1, n) :
+    for col in range(1, n) : 
+        arr2[0][col] = arr2[0][col - 1] + arr1[0][col] 
+    arr2[row][0] = arr2[row - 1][0] + arr1[row][0]
 
-    print(arr2)
+# 나머지 값 채우기
+for row in range(1, n) :
+    for col in range(1, n) : 
+        arr2[row][col] = max(arr2[row - 1][col] + arr1[row][col], arr2[row][col - 1] + arr1[row][col])
+
+print(max(arr2[-1]))

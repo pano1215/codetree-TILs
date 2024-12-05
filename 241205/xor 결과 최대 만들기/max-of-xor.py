5ba5d0bdce444c90eb271197deed1693391c575c
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-12-05 13:30:45 +0900

    Title: xor 결과 최대 만들기, Time: 5032ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 241205/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+exp = list(map(int, input().split()))
+
+temp = []
+max_num = -1
+
+def find_max(result) :
+    global max_num
+    max_num = max(max_num, result)
+
+def xor() :
+    result = 0
+    for t in temp :
+        result = result ^ t
+    find_max(result)
+    return 
+
+def start(num) :
+    if len(temp) == m :
+        xor()
+        return 
+    
+    for i in range(num, n) :
+        temp.append(exp[i])
+        start(num + 1)
+        temp.pop()
+
+start(0)
+
+print(max_num)

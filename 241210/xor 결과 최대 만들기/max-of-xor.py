1330c1f5dfff18c646a2930d6a957d3135e4f83e
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-12-10 15:40:38 +0900

    Title: xor 결과 최대 만들기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241210/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,14 @@
+from itertools import combinations 
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+max_num = -1
+result = 1 
+for com in combinations(arr, m) :
+    #print(com)
+    for num in com :
+        result = result ^ num
+    max_num = max(max_num, result)
+    result = 1 
+print(max_num)

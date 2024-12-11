d09e81929f4caede0f120bae1e173083ab0934db
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-12-11 11:25:59 +0900

    Title: 최소 점프 횟수, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241211/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+def min_jump(n, arr) : 
+    visited = [False] * n
+    q = deque([(0, 0)])
+
+    visited[0] = True
+
+    while q :
+        position, jump_count = q.popleft()
+
+        for i in range(position + 1, position + arr[position] + 1) :
+            #print('i, arr[i], arr[position], jump_count : ', i, arr[i], arr[position], jump_count)
+
+            if i == n - 1 :
+                return jump_count + 1 
+            
+            if not visited[i] :
+                visited[i] = True
+                q.append((i, jump_count + 1))
+            #print(visited, q)
+    return -1
+
+print('result : ', min_jump(n, arr))

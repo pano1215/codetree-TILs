79075cb1f9d9f31309d94f12eb273f76a06a61b7
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-12-12 15:03:43 +0900

    Title: n개의 점 중 m개 고르기, Time: 71ms, Memory: 18MB, Status: Passed - CodeTree

--- 241212/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 241212/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -10,7 +10,6 @@ def min_length(com) :
     global max_len
 
     max_len = -sys.maxsize
-    #print('com : ', com)
     for i in range(len(com)) :
         x1, y1 = com[i]
         for j in range(i + 1, len(com)) :
@@ -18,16 +17,13 @@ def min_length(com) :
             leng = (x1 - x2) ** 2 + (y1 - y2) ** 2
             
             max_len = max(max_len, leng)
-            #print('leng : ', leng, max_len)
     max_arr.append(max_len)
     
     return max_len
 
 max_arr = []
 for com in combinations(arr, m) :
-    #print(com)
     max_len = min_length(com)
     max_arr.append(max_len)
-#print('max_arr : ', max_arr)
 
 print(min(max_arr))

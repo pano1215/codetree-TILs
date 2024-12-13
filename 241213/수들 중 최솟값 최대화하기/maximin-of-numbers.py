3c2a258e21a64146bfb314cd8e93bfcace11efec
Cindy Han <78583863+pano1215@users.noreply.github.com>
2024-12-13 15:54:35 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 249ms, Memory: 23MB, Status: Passed - CodeTree

--- 241213/수들 중 최솟값 최대화하기/maximin-of-numbers.py
+++ 241213/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -6,18 +6,19 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [False] * n
 pick = [] 
 
-min_val = sys.maxsize
 max_val = -sys.maxsize 
 
 def start(row) :
     global min_val, max_val
 
     if row == n :
-        sum_val = 0
-        for num in pick :
-            sum_val += num
-        min_val = min(min_val, sum_val)
-
+        min_val = sys.maxsize
+        for num in range(len(pick)) :
+            #sum_val += arr[num][pick[num]]
+            
+            min_val = min(min_val, arr[num][pick[num]])
+            #print('arr[num][pick[num]] : ', arr[num][pick[num]], min_val)
+        #print('갱신 끝')
         return min_val
 
     for i in range(n) :
@@ -26,13 +27,14 @@ def start(row) :
         
         visited[i] = True
         pick.append(i)
-
+        #print('pick : ', pick)
+        #print('visited : ', visited)
         start(row + 1)
 
         visited[i] = False
         pick.pop()
 
-    max_val = max(min_val, max_val)
+        max_val = max(min_val, max_val)
 
 start(0)
 print(max_val)

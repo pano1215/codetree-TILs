cb529b93bb2cf90340a72e0b97341c3f5b666732
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-05-30 16:08:52 +0900

    Title: 알바로 부자 되기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250530/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,19 @@
+n = int(input())
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
+jobs.sort()
+
+dp = [0] * n
+
+for i in range(n) :
+    dp[i] = jobs[i][2]
+
+    for j in range(i) : 
+        i_str_day = jobs[i][0]
+        j_end_day = jobs[j][1]
+
+        #print(i_str_day, j_end_day)
+        if j_end_day < i_str_day :
+            dp[i] = max(dp[i], dp[j] + jobs[j][2])
+            #print(dp)
+
+print(max(dp))

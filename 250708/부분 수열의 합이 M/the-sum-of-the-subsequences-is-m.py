c51b9af8a46757cffec2a039023798e7213bb33b
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-08 11:26:49 +0900

    Title: 부분 수열의 합이 M, Time: 74ms, Memory: 17MB, Status: Passed - Codetree

--- 250708/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 250708/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -1,21 +1,18 @@
 import sys
 
-n, m = map(int, input().split())
+n, m = map(int, input().split()) # 원소 수, 합 
 arr = list(map(int, input().split()))
 
+max_size = sys.maxsize
+dp = [max_size for _ in range(m + 1)]
+dp[0] = 0  
 
-max_val = sys.maxsize
-dp = [0] * (m + 1)
-for i in range(m + 1) : 
-    dp[i] = max_val
-dp[0] = 0
-
-for i in range(n) : # 수열의 원소만큼 반복 
-    for j in range(m, -1, - 1) : 
+for i in range(n) : 
+    for j in range(m, -1, -1) : 
         if j >= arr[i] : 
             dp[j] = min(dp[j], dp[j - arr[i]] + 1)
 
-if dp[m] == max_val :
+if dp[m] == max_size : 
     print(-1)
 else : 
     print(dp[m])

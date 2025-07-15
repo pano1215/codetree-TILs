b6c44ae131cc2f59e6c6f0027ce1c7e5222b03ad
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-15 23:51:25 +0900

    Title: 배낭 채우기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250715/배낭 채우기/knapsack.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split()) # 보석 수, 무게 
+w, v = zip(*[tuple(map(int, input().split())) for _ in range(n)])
+w, v = list(w), list(v)
+
+dp = [0 for _ in range(m + 1)]
+
+for i in range(n) : # 가방
+    for j in range(m, w[i] -1, -1) : # 무게
+        dp[j] = max(dp[j], dp[j - w[i]] + v[i])
+
+print(max(dp))

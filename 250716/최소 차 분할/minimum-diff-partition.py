eceb11b1041befde02ba08fe0806eedec43d8d2f
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-16 00:31:43 +0900

    Title: 최소 차 분할, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250716/최소 차 분할/minimum-diff-partition.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+total_sum = sum(arr)
+max_sum = total_sum // 2
+
+dp = [False] * (max_sum + 1)
+dp[0] = True
+
+for i in arr : # 제시된 숫자들을 순회함 
+    for j in range(max_sum, i - 1, -1) : # dp를 순회함(이때 dp의 idx는 각 집단의 합을 의미함). 중복이 없도록 순회함 
+        dp[j] |= dp[j - i]
+    #print(dp)
+
+min_val = max_sum
+for i in range(max_sum, -1, -1) : 
+    if dp[i] : 
+        min_val = total_sum - i * 2
+        print(min_val)
+        break 

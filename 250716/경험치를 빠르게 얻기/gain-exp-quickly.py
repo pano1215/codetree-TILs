d0c9d1f063fe31b65b96dedb7a8911503da8e62f
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-16 23:06:09 +0900

    Title: 경험치를 빠르게 얻기, Time: 155ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 250716/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, input().split()) # n : 퀘스트 정보, m : 총 합 
+quests = [tuple(map(int, input().split())) for _ in range(n)]
+
+# dp[i] = i시간 동안 얻을 수 있는 최대 경험치
+dp = [-1] * (m + 1)
+
+for exp, time in quests :  # 순서 주의! (exp, time)
+    for i in range(m, time - 1, -1) :
+        dp[i] = max(dp[i], dp[i - time] + exp)
+
+#print(dp)
+min_val = -sys.maxsize 
+for i in range(1, len(dp)) : 
+    if dp[i - 1] < dp[i] : 
+        min_val = max(i, min_val)
+print(min_val)

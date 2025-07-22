49a550cc5b7ee4ee4d80897a7a83af5f4ecdb507
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-23 01:57:46 +0900

    Title: 동일하게 분할, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250723/동일하게 분할/equal-partition.py
@@ -0,0 +1,26 @@
+import sys
+n = int(input())
+arr = list(map(int, input().split()))
+
+total_sum = sum(arr)
+
+if total_sum % 2 != 0:
+    print('No')
+    
+    sys.exit()
+
+avg_sum = total_sum // 2
+
+dp = [False] * (avg_sum + 1)
+dp[0] = True
+
+for num in arr :
+    for i in range(avg_sum, num - 1, -1) :
+        dp[i] |= dp[i - num]
+
+        print(dp)
+
+if not dp[avg_sum]:
+    print('No')
+    
+    sys.exit()

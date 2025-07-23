df5b8581d810358e481342cf21f22a7e2c05a8f9
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-07-23 23:21:42 +0900

    Title: 동일하게 분할, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/동일하게 분할/equal-partition.py
+++ 250723/동일하게 분할/equal-partition.py
@@ -6,7 +6,6 @@ total_sum = sum(arr)
 
 if total_sum % 2 != 0:
     print('No')
-    
     sys.exit()
 
 avg_sum = total_sum // 2
@@ -17,10 +16,9 @@ dp[0] = True
 for num in arr :
     for i in range(avg_sum, num - 1, -1) :
         dp[i] |= dp[i - num]
-
-        print(dp)
+        #print(dp)
 
 if not dp[avg_sum]:
     print('No')
-    
-    sys.exit()
+else : 
+    print('Yes')

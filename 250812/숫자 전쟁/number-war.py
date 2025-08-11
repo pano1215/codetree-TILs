402fecebb734e856d47cca21b140833c6ca0276d
Cindy Han <78583863+pano1215@users.noreply.github.com>
2025-08-12 00:15:49 +0900

    Title: 숫자 전쟁, Time: 59ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250812/숫자 전쟁/number-war.py
@@ -0,0 +1,15 @@
+n = int(input())
+first_cards = [0] + list(map(int, input().split()))
+second_cards = [0] + list(map(int, input().split()))
+
+dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for i in range(1, n + 1) : 
+    for j in range(1, n + 1) : 
+        if second_cards[i] < first_cards[j] : 
+            dp[i][j] = dp[i - 1][j] + second_cards[i]
+
+result = 0
+for i in range(n + 1) : 
+    result = max(dp[i])
+print(result)
